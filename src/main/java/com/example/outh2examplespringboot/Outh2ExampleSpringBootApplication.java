e70b4ebb353953d45ce4797c7183d36aa8aa2a0a
GiorgiAbzianidzeSE <giorgiabzianidze26@gmail.com>
2023-09-08 16:15:26 +0400

    git commit

---
+++ src/main/java/com/example/outh2examplespringboot/Outh2ExampleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.outh2examplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Outh2ExampleSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Outh2ExampleSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/outh2examplespringboot/Outh2ExampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.outh2examplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Outh2ExampleSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
